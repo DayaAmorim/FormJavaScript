ad44b6e8fd9809a280df7cd4ca55b50020bbfaca
Daya Amorim <138938193+DayaAmorim@users.noreply.github.com>
2023-08-31 22:14:17 -0300

    Add files via upload

---
+++ js/matiz.js
@@ -0,0 +1,11 @@
+/let meuArray = ['dayaneamorim2010@gmail.com','victoramorimdesousa2002@gmail.com'];
+
+console.table(meuArray);
+//"Daya", "Victor", "William", "Asta"];
+
+
+if meuArray =('dayaneamorim2010@gmail.com','victoramorimdesousa2002@gmail.com')
+return alert = ('Seu acesso foi autorizado com sucesso!')
+
+
+
